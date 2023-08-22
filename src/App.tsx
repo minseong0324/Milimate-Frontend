b68afc4de91bb08829241e6c4c2317dc70e81bf9
minseong0324 <kwan0324@naver.com>
2023-08-22 19:20:41 +0900

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,65 @@
-import { useState } from 'react'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import VisitorHome from './pages/home/VisitorHome/VisitorHome';
+import Home from './pages/home/Home';
+import UserProvider from './contexts/UserProvider/UserProvider';
+import Login from './pages/login/Login';
+import SignUp from './pages/signup/SignUp';
+import HomeBeforeLogin from './pages/home/HomeBeforeLogin/HomeBeforeLogin';
+import OwnerHome from './pages/home/OwnerHome/OwnerHome';
+import useScroll from './hooks/useScroll/useScroll';
+import LeafFalling from './components/leafFalling/leafFalling'
+import Clouds from './components/Clouds/Clouds';
+import SelectTreeCharacter from './pages/select/selectTreeCharacter';
+import KakaoLogin from './pages/login/kakaoLogin/KakaoLogin';
+import KakaoCallback from './pages/login/kakaoLogin/KakaoCallback';
+import NaverLogin from './pages/login/naverLogin/NaverLogin';
+import NaverCallback from './pages/login/naverLogin/NaverCallback';
+import GoogleLoginButton from './pages/login/googleLogin/GoogleLoginButton';
+import GoogleCallback from './pages/login/googleLogin/GoogleCallback';
+import MyPage from './pages/MyPage/MyPage';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import TokenProvider from './contexts/TokenProvider/TokenProvider';
+
+const queryClient = new QueryClient();
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+    
+    <QueryClientProvider client={queryClient}>
+     
+    <UserProvider>
+      <LeafFalling />
+      <Clouds />
+      
+      <Router>
+      <TokenProvider>
+        <Routes>
+        
+          <Route path="/" element={<HomeBeforeLogin />} />
+          <Route path="/home/:userId" element={<Home />} />
+          <Route path="/ownerhome" element={<OwnerHome />} /> {/* 이건 출시 이전에 뺄 것입니다. 개발을 위해 라우트 해두었습니다. */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/select-character-tree/:userId" element={<SelectTreeCharacter />} />
+        
+        
+          <Route path="/kakao/callback" element={<KakaoCallback />} />
+        
+          <Route path="/naver/callback" element={<NaverCallback />} />
+          <Route path="/google/callback" element={<GoogleCallback />} />
 
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+          <Route path="/mypage/:userId" element={<MyPage />} />
+          
+        </Routes>
+        </TokenProvider>
+      </Router>
+    </UserProvider>
+    </QueryClientProvider>
+    
     </>
-  )
+    
+  );
 }
 
-export default App
+export default App;
