96b9c52faf9a3adb35fe6805a19ffcd9bdabeb40
keoni <kimkeonhwi991231@gmail.com>
2023-08-28 22:43:16 +0900

    hmr

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,19 @@
-import {defineConfig} from 'vite';
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import viteTsconfigPaths from 'vite-tsconfig-paths';
-import svgrPlugin from 'vite-plugin-svgr';
+import viteTsconfigPaths from "vite-tsconfig-paths";
+import svgrPlugin from "vite-plugin-svgr";
 
 export default defineConfig({
-  plugins: [react(), viteTsconfigPaths(), svgrPlugin(), ],
+  plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
   resolve: {
     alias: {
-      '@vitejs/plugin-react': '@vitejs/plugin-react/dist/index.es.js'
-    }
+      "@vitejs/plugin-react": "@vitejs/plugin-react/dist/index.es.js",
+    },
   },
-  server: { 
-    open: true, 
-    port: 3000, 
+  server: {
+    open: true,
+    port: 3000,
+    hmr: false,
   },
-  define: { global: 'window' },
+  define: { global: "window" },
 });
