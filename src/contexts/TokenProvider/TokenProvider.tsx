e22b4cfec2613a51b770ab25f77c76dfdc539bde
minseong0324 <kwan0324@naver.com>
2023-09-27 06:19:05 +0900

    accessToken

--- src/contexts/TokenProvider/TokenProvider.tsx
+++ src/contexts/TokenProvider/TokenProvider.tsx
@@ -60,7 +60,6 @@ function TokenProvider({ children }: TokenProviderProps) {
                       }
                     } 
                     setErrorModalOpen(true)
-                    localStorage.clear();
                 
             }
         
