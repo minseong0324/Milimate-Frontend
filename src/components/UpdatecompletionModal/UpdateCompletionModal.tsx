520d38f5e3182303be641ff7929c65aae6c44c70
keoni <kimkeonhwi991231@gmail.com>
2023-09-27 13:53:00 +0900

    mypage completion edit close modal

--- src/components/UpdatecompletionModal/UpdateCompletionModal.tsx
+++ src/components/UpdatecompletionModal/UpdateCompletionModal.tsx
@@ -6,6 +6,8 @@ import { updateCompletionDate } from "../Redux/Slices/userInfoSlice";
 import { RootState } from "../Redux/store";
 import { useToken } from "../../contexts/TokenProvider/TokenProvider";
 import SmallModal from "../../components/ErrorModal/ErrorModal"
+import {Simulate} from "react-dom/test-utils";
+import error = Simulate.error;
 
 interface PropsType {
   setModalOpen: (open: boolean) => void;
@@ -21,12 +23,14 @@ function UpdateCompletionModalBasic({ setModalOpen }: PropsType) {
   const [isSmallModalOpen, setSmallModalOpen] = useState(false);
   const [modalSmallContent, setModalSmallContent] =
     useState<React.ReactNode>(null); // 모달에 표시될 내용을 저장
-
+  const [errorState, setErrorState] = useState(2);
   // 모달 끄기
   const closeModal = () => {
     setModalOpen(false);
   };
-
+  if(errorState == 1) {
+    setModalOpen(false);
+  }
   useEffect(() => {
     setSmallModalOpen(true)
     setModalSmallContent(
@@ -93,45 +97,48 @@ function UpdateCompletionModalBasic({ setModalOpen }: PropsType) {
 
 
     if (
-      !userInfo.enlistmentYear ||
-      !userInfo.enlistmentMonth ||
-      !userInfo.enlistmentday
+        !userInfo.enlistmentYear ||
+        !userInfo.enlistmentMonth ||
+        !userInfo.enlistmentday
     ) {
+      setErrorState(1);
       return alert("필수 정보를 입력해주세요!");
     }
 
     // Case 2: 입대일과 수료일을 입력하는 모든 값들은 정수여야 한다.
     if (
-      !isNumeric(userInfo.enlistmentYear) ||
-      !isNumeric(userInfo.enlistmentMonth) ||
-      !isNumeric(userInfo.enlistmentday) ||
-      (completionYear && !isNumeric(completionYear)) ||
-      (completionMonth && !isNumeric(completionMonth)) ||
-      (completionDay && !isNumeric(completionDay))
+        !isNumeric(userInfo.enlistmentYear) ||
+        !isNumeric(userInfo.enlistmentMonth) ||
+        !isNumeric(userInfo.enlistmentday) ||
+        (completionYear && !isNumeric(completionYear)) ||
+        (completionMonth && !isNumeric(completionMonth)) ||
+        (completionDay && !isNumeric(completionDay))
     ) {
+      setErrorState(1);
       return alert("날짜를 숫자로 입력해주세요!");
     }
 
     // Case 4: 입대일의 달과 수료일의 달은 1 ~ 12의 값들을 가지고 있어야 한다.
     if (
-      parseInt(userInfo.enlistmentMonth) < 1 ||
-      parseInt(userInfo.enlistmentMonth) > 12 ||
-      (completionMonth &&
-        (parseInt(completionMonth) < 1 || parseInt(completionMonth) > 12))
+        parseInt(userInfo.enlistmentMonth) < 1 ||
+        parseInt(userInfo.enlistmentMonth) > 12 ||
+        (completionMonth &&
+            (parseInt(completionMonth) < 1 || parseInt(completionMonth) > 12))
     ) {
+      setErrorState(1);
       return alert("월은 1 ~ 12 사이의 값이어야 합니다!");
     }
 
     // Case 1: 입대일은 수료일보다 미래일 수 없다.
     const enlistDate = new Date(
-      parseInt(userInfo.enlistmentYear),
-      parseInt(userInfo.enlistmentMonth) - 1,
-      parseInt(userInfo.enlistmentday)
+        parseInt(userInfo.enlistmentYear),
+        parseInt(userInfo.enlistmentMonth) - 1,
+        parseInt(userInfo.enlistmentday)
     );
     const completeDate = new Date(
-      parseInt(completionYear),
-      parseInt(completionMonth) - 1,
-      parseInt(completionDay)
+        parseInt(completionYear),
+        parseInt(completionMonth) - 1,
+        parseInt(completionDay)
     );
 
     if (completeDate < enlistDate) {
@@ -141,17 +148,17 @@ function UpdateCompletionModalBasic({ setModalOpen }: PropsType) {
 
     // Case 5: 수료일은 필수값이 아니기 때문에 입력하는 칸 3개 중에서 하나라도 잘못하면 서버한테 completionYear, completionMonth, completionDay를 전부 "0"으로 전송한다.
     if (
-      !completionYear ||
-      !completionMonth ||
-      !completionDay ||
-      !isNumeric(completionYear) ||
-      !isNumeric(completionMonth) ||
-      (completionDay && !isNumeric(completionDay)) || // completionDay가 있을 때만 숫자인지 확인
-      parseInt(completionMonth) < 1 ||
-      parseInt(completionMonth) > 12 ||
-      (completionDay &&
-        parseInt(completionDay) >
-          getMaxDayOfMonth(parseInt(completionMonth), parseInt(completionYear))) // completionDay가 있을 때만 해당 월의 최대 일 수를 초과하는지 확인
+        !completionYear ||
+        !completionMonth ||
+        !completionDay ||
+        !isNumeric(completionYear) ||
+        !isNumeric(completionMonth) ||
+        (completionDay && !isNumeric(completionDay)) || // completionDay가 있을 때만 숫자인지 확인
+        parseInt(completionMonth) < 1 ||
+        parseInt(completionMonth) > 12 ||
+        (completionDay &&
+            parseInt(completionDay) >
+            getMaxDayOfMonth(parseInt(completionMonth), parseInt(completionYear))) // completionDay가 있을 때만 해당 월의 최대 일 수를 초과하는지 확인
     ) {
       setcompletionYear("0");
       setcompletionMonth("0");
@@ -167,6 +174,7 @@ function UpdateCompletionModalBasic({ setModalOpen }: PropsType) {
     // alert(completeDate);
     // alert(enlistDate);
     if (completeDate <= enlistDate) {
+      setErrorState(1);
       return alert("수료일은 현재 날짜보다 미래여야 합니다!");
     }
 
@@ -174,61 +182,61 @@ function UpdateCompletionModalBasic({ setModalOpen }: PropsType) {
     const completionDayInt = parseInt(completionDay);
 
     if (
-      enlistmentDayInt >
+        enlistmentDayInt >
         getMaxDayOfMonth(
-          parseInt(userInfo.enlistmentMonth),
-          parseInt(userInfo.enlistmentYear)
+            parseInt(userInfo.enlistmentMonth),
+            parseInt(userInfo.enlistmentYear)
         ) ||
-      completionDayInt >
+        completionDayInt >
         getMaxDayOfMonth(parseInt(completionMonth), parseInt(completionYear))
     ) {
+      setErrorState(1);
       return alert("입력한 날짜가 해당 달의 최대 일 수를 초과하였습니다!");
     }
-
-    try {
-      const response = await axios.put(
-        `https://api.mili-mate.com/api/myPage/${userId}/editCompletion`,
-        {
-          completionYear: completionYear,
-          completionMonth: completionMonth,
-          completionDay: completionDay,
-        },
-        {
-          headers: { //
-            authorization: `${accessToken}`,
-          },
-        }
-      );
-      if (response.status == 200) {
-        alert("수료일을 수정했습니다.");
-        dispatch(
-          updateCompletionDate({
-            completionYear: completionYear,
-            completionMonth: completionMonth,
-            completionDay: completionDay,
-          })
+    if (errorState == 0) {
+      try {
+        const response = await axios.put(
+            `https://api.mili-mate.com/api/myPage/${userId}/editCompletion`,
+            {
+              completionYear: completionYear,
+              completionMonth: completionMonth,
+              completionDay: completionDay,
+            },
+            {
+              headers: { //
+                authorization: `${accessToken}`,
+              },
+            }
         );
-        closeModal();
-      }
-    } catch (error: unknown) {
-      //에러 일 경우
-      if (error instanceof AxiosError) {
-        const status = error?.response?.status;
-
-
-        if (status === 404) {
-          // 리소스를 찾을 수 없음
-        } else if (status === 500) {
-          // 서버 내부 오류
-        } else {
-          // 기타 상태 코드 처리
+        if (response.status == 200) {
+          alert("수료일을 수정했습니다.");
+          dispatch(
+              updateCompletionDate({
+                completionYear: completionYear,
+                completionMonth: completionMonth,
+                completionDay: completionDay,
+              })
+          );
+          closeModal();
         }
+      } catch (error: unknown) {
+        //에러 일 경우
+        if (error instanceof AxiosError) {
+          const status = error?.response?.status;
+          if (status === 404) {
+            // 리소스를 찾을 수 없음
+          } else if (status === 500) {
+            // 서버 내부 오류
+          } else {
+            // 기타 상태 코드 처리
+          }
+        }
+
+        return alert(`Failed to fetch user info ${error}`);
       }
-
-      return alert(`Failed to fetch user info ${error}`);
     }
-  };
-
+    ;
+  }
   const userInfo = useSelector((state: RootState) => state.userInfo);
   return (
     <SmallModal isOpen={isSmallModalOpen} onClose={() => setSmallModalOpen(false)} >
