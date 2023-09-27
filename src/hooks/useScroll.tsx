9e9559fa30e1cd0860bbbe1f371374b8d11f2297
minseong0324 <kwan0324@naver.com>
2023-09-27 20:03:50 +0900

    dplt 33

--- src/hooks/useScroll.tsx
+++ src/hooks/useScroll.tsx
@@ -5,7 +5,7 @@ const useScroll = (): [React.MutableRefObject<HTMLDivElement | null>, boolean] =
   const ref = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
-    
+
     const handleScroll = () => {
       if (!ref.current) return;
       const topPos = window.pageYOffset + window.innerHeight;
@@ -23,6 +23,8 @@ const useScroll = (): [React.MutableRefObject<HTMLDivElement | null>, boolean] =
     if (ref.current) observer.observe(ref.current);
 
     return () => {
+      
+      
       if (ref.current) observer.unobserve(ref.current);
     };
   }, []); // 빈 배열 대신 필요한 의존성을 명시해야 할 수도 있음
