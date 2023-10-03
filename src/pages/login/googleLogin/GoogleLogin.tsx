bbf14502dab9de187beb5c873d558bbce52ea686
minseong0324 <kwan0324@naver.com>
2023-10-03 20:15:34 +0900

    구글로그인 막음

--- src/pages/login/googleLogin/GoogleLogin.tsx
+++ src/pages/login/googleLogin/GoogleLogin.tsx
@@ -10,8 +10,8 @@ const GoogleLogin: React.FC<GoogleLoginButtonProps> = ({ buttonImage }) => {
 
   // 이미지 클릭 시 네이버 로그인 페이지로 이동
   const handleButtonClick = () => {
-    window.location.href = "https://api.mili-mate.com/api/oauth/google";
-    //alert("구글 로그인은 10/9 오픈예정입니다.")
+    //window.location.href = "https://api.mili-mate.com/api/oauth/google";
+    alert("구글 로그인은 10/9 오픈예정입니다. 카카오 로그인을 이용해주세요.")
   };
 
   return (
